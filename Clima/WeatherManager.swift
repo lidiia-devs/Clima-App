468e887ef9095a154a643e4de259df6db27b77fa
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-04-28 17:04:21 -0400

    handler added

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -14,9 +14,52 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
     
+    func performRequest(urlString: String) {
+        //Networking steps:
+        //1.create a url
+        if let url = URL(string: urlString) {
+            //2. create a URLSession
+            let session = URLSession(configuration: .default)
+            //3. give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                {
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    if let safeData = data {
+                        let dataString = String(data: safeData, encoding: .utf8)
+                        print(dataString)
+                        
+                    }
+                }
+            }
+            //let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            
+            //4. start the task
+            task.resume()
+        }
+    }
+    
+    //func for task's completionHandler
+    //    func handle(data: Data?, response: URLResponse?, error: Error?) {
+    //        if error != nil {
+    //            print(error!)
+    //            return
+    //        }
+    //
+    //        if let safeData = data {
+    //            let dataString = String(data: safeData, encoding: .utf8)
+    //            print(dataString)
+    //
+    //        }
+    //    }
 }
+
 
 //9d766e2cc5ba0a8743215d778887930c
